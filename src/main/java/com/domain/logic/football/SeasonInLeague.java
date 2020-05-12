7e8bdceb95998b985d9fe59eef2aa1f87380d4fb
nivgold <nivgold@post.bgu.ac.il>
2020-05-12 22:52:30 +0300

    added league to ddl sql script

---
+++ src/main/java/com/domain/logic/football/SeasonInLeague.java
@@ -0,0 +1,17 @@
+package com.domain.logic.football;
+
+import com.domain.logic.policies.Policy;
+import com.domain.logic.roles.Referee;
+
+import java.util.ArrayList;
+
+public class SeasonInLeague {
+    private League league;
+    private Season season;
+    private Policy policy;
+    private ArrayList<Team> teams;
+    private ArrayList<Referee> referees;
+
+    public SeasonInLeague() {
+    }
+}
